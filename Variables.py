7a67c2a17577d8cf0d8ee2f002987632dcbf051a
pearl011ab <pearlaboagye8@gmail.com>
2025-06-05 19:14:52 +0000

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,34 @@
+x = 6
+y = "Pearl"
+
+m = str(3)
+n = int(3)
+o = float(3)
+
+"""
+'type()' is used to get the data type of a variable
+"""
+print(m, type(m))
+print(n, type(n))
+print(o, type(o))
+
+print("Data type of 'y' variable is: ", type(y))
+
+my_var = 8
+
+pearlKusiwaa = 34 # camelCase
+PearlKusiwaa = 8 # PascalCase
+pearl_kusiwaa = "@dkfa" # snake_case
+
+animals = ['cat', 'dog', 'goat']
+r, f, s = animals
+print(r, f, s)
+# Unpacking a collection 
+
+P = "I "
+A = "am "
+S = "Shalom"
+number = 3
+concat = P + A + S # concatenating strings
+print(P + A + S, number)
+print(concat)
